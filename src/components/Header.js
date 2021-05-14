7bae481f06a84ef2d49e60d12545bbc5c3f020a5
Randy Ortiz <Randyortiz28@gmail.com>
2021-05-13 20:28:04 -0400

    Changed name to Punk-beer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
-    return (
-        <div>
-            <header className="header">
-                <h1>Randy's Pub</h1>
-                <ul className="list">
-                    <NavLink exact to='/' 
-                    activeStyle={{
-                        fontWeight: "bold",
-                        color: "#755eea"
-                    }}
-                    >Home
-                    </NavLink>
-                    <NavLink exact to='/beer'
-                    activeStyle={{
-                        fontWeight: "bold",
-                        color: "#755eea"
-                    }}
-                    >Beers</NavLink>
-                </ul>
-            </header>
-        </div>
-    );
+  return (
+    <div>
+      <header className="header">
+        <h1>Punk Beer</h1>
+        <ul className="list">
+          <NavLink
+            exact
+            to="/"
+            activeStyle={{
+              fontWeight: 'bold',
+              color: '#755eea',
+            }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            exact
+            to="/beer"
+            activeStyle={{
+              fontWeight: 'bold',
+              color: '#755eea',
+            }}
+          >
+            Beers
+          </NavLink>
+        </ul>
+      </header>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
